fd6d45a2af98ecf0d784225111094854be113053
arachnothrone <arachnothrone@gmail.com>
2021-05-06 00:12:30 -0400

    Update iOS application: new commands

--- ios/denon_remote/denon_remote/ContentView.swift
+++ ios/denon_remote/denon_remote/ContentView.swift
@@ -10,28 +10,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Text("Denon Remote")
-            .font(.title)
-            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-            .padding()
-            .background(Color.purple)
-            .foregroundColor(.white)
-            .padding(10)
-            .border(Color.purple, width: 5)
-        VStack {
-            Button(action: {udpSendString(textToSend: "CMD01POWER_____", address: "192.168.2.101", port: 19001)}, label: {
-                    Text("Power ON/OFF").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).foregroundColor(.gray).padding()})
+//        Text("Denon Remote")
+//            .font(.title)
+//            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+//            .padding()
+//            .background(Color.purple)
+//            .foregroundColor(.white)
+//            .padding(10)
+//            .border(Color.purple, width: 5)
+        VStack (alignment: .center, spacing: 5){
+            Text("Denon Remote")
+                .font(.title)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(Color.purple)
+                .foregroundColor(.white)
+                .padding(10)
+                .border(Color.purple, width: 5)
+            Text(" ").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            
             HStack {
-                Button(action: {udpSendString(textToSend: "CMD02VOLUMEUP__", address: "192.168.2.101", port: 19001)}, label: {
-                    HStack {
-                        Image(systemName: "arrowtriangle.up.fill").foregroundColor(.orange).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                        Text("Up").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    }
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color.red)
-                    .cornerRadius(40)
-                })
+                Button(action: {udpSendString(textToSend: "CMD04POWERON", address: "192.168.2.101", port: 19001)}, label: {
+                        Text("Power ON").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).foregroundColor(.gray).padding()})
+                Button(action: {udpSendString(textToSend: "CMD05POWERON", address: "192.168.2.101", port: 19001)}, label: {
+                        Text("Power OFF").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).foregroundColor(.gray).padding()})
+            }
+            HStack {
                 Button(action: {udpSendString(textToSend: "CMD03VOLUMEDOWN", address: "192.168.2.101", port: 19001)}, label: {
                     HStack {
                         Text("Down").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
@@ -42,8 +46,57 @@ struct ContentView: View {
                     .background(Color.yellow)
                     .cornerRadius(40)
                 })
+                Button(action: {udpSendString(textToSend: "CMD02VOLUMEUP__", address: "192.168.2.101", port: 19001)}, label: {
+                    HStack {
+                        Image(systemName: "arrowtriangle.up.fill").foregroundColor(.orange).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        Text("Up").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    }
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.red)
+                    .cornerRadius(40)
+                })
             }
-            Button(action: {udpSendString(textToSend: "CMD04DIMMER", address: "192.168.2.101", port: 19001)}) {
+            
+            Text(" ").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            Text("-= Stereo Settings =-").foregroundColor(.black)
+            HStack {
+                Button(action: {udpSendString(textToSend: "CMD09STANDARD", address: "192.168.2.101", port: 19001)}, label: {
+                    Text("STANDARD").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).foregroundColor(.red)
+                })
+                Button(action: {udpSendString(textToSend: "CMD12DIRECT", address: "192.168.2.101", port: 19001)}, label: {
+                    Text("DIRECT").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).foregroundColor(.green)
+                })
+                Button(action: {udpSendString(textToSend: "CMD13STEREO", address: "192.168.2.101", port: 19001)}, label: {
+                    Text("STEREO").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).foregroundColor(.blue)
+                })
+                Button(action: {udpSendString(textToSend: "CMD075CH7CHSTEREO", address: "192.168.2.101", port: 19001)}, label: {
+                    Text("5ch/7ch").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).foregroundColor(.purple)
+                })
+                
+            }
+            
+            Text(" ").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            Button(action: {udpSendString(textToSend: "CMD06MUTE", address: "192.168.2.101", port: 19001)}, label: {
+                HStack {
+                    Text("Mute").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    Image(systemName: "speaker.slash").background(Color.clear).foregroundColor(.blue).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }
+                //.border(Color.purple, width: 5)
+                .padding()
+                .foregroundColor(.gray)
+                .background(Color.clear)
+                //.border(Color.purple, width: 5)
+                .cornerRadius(20)
+                //.padding()
+                //.border(Color.purple, width: 5)
+                .overlay(
+                        RoundedRectangle(cornerRadius: 40)
+                            .stroke(Color.gray, lineWidth: 5)
+                    )
+            })
+            
+            Button(action: {udpSendString(textToSend: "CMD01DIMMER", address: "192.168.2.101", port: 19001)}) {
                 HStack {
                     //Text("Dimmer").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                     Image(systemName: "rays").foregroundColor(.green).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).padding()
