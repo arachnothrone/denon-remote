e900bacc9151b6ad8e78341587b303405933247a
arachnothrone <arachnothrone@gmail.com>
2021-06-17 23:14:03 -0400

    WatchOS: init

---
+++ wos/denon_IR/denon_IR WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  denon_IR WatchKit Extension
+//
+//  Created by Alex Fadeev on 2021-06-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wos/denon_IR/denon_IR WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  denon_IR WatchKit Extension
+//
+//  Created by Alex Fadeev on 2021-06-17.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
