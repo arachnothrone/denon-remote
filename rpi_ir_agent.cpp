b7a656ca8054aa4f9730283dc8851f968c679b56
arachnothrone <arachnothrone@gmail.com>
2022-01-16 22:56:58 -0500

    RPi IR-agent v2, init

---
+++ rpi_ir_agent.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file rpi_ir_agent.cpp
+ * @author arachnothrone
+ * @brief 
+ * @version 2.1
+ * @date 2022-01-16
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class RequestMessage {
+    private:
+        /* data */
+    public:
+        RequestMessage (/* args */);
+        ~RequestMessage ();
+};
+
+class ResponseMessage {
+    public:
+        //
+    private:
+        //
+};
+
+class Denon {
+    public:
+        //
+    private:
+        //
+};
+
