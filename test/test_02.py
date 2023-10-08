2fca622305381467a91ad81a025581b4263bb78b
arachnothrone <arachnothrone@gmail.com>
2023-10-07 21:39:37 -0400

    Add test Mute toggle

--- test/test_02.py
+++ test/test_02.py
@@ -31,4 +31,34 @@ def test_POWER_ON_OFF():
         assert response in stdout.decode()
 
     server_thread.join()
+    time.sleep(2)
 
+
+def test_MUTE_TOGGLE():
+    # start server
+    server_command = ["../rpi_ag.out"]
+    server_args = ["-h 19", "-m 20", "-s 22", "-v -42"]
+    server_thread = threading.Thread(target=tp.run_process, args=(server_command, server_args))
+
+    server_thread.start()
+
+    client_command = ["python3", "../client.py"]
+    client_args = ["127.0.0.1", "19001"]
+    test_commands = ["CMD04POWERON", "CMD06MUTE", "CMD06MUTE", "CMD88SHUTDOWN"]
+    test_responses = ["1,-42,0,2,0,0,1", "1,-42,1,2,0,0,1", "1,-42,0,2,0,0,1", "0,-42,0,2,0,0,1"]
+
+    for command, response in zip(test_commands, test_responses):
+        cmd_cliargs = [command] + client_args
+        stdout, stderr = tp.run_command(client_command, cmd_cliargs, delay=1)
+
+        if stdout:
+            print("Process Output:", stdout)
+        if stderr:
+            print("Process Error:", stderr)
+
+        assert command in stdout.decode()
+        assert response in stdout.decode()
+
+    server_thread.join()
+    time.sleep(2)
+    
