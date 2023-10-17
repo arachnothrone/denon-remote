cfb4b00e3bf54665929cc1b200aa83882ba9a511
arachnothrone <arachnothrone@gmail.com>
2023-10-17 18:39:33 -0400

    Kill script for server instances

---
+++ test/killservers.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Kills all local running servers.
+
+binlist=("rpi_ag.out" "ir_agent.out")
+
+# Check names in cli arguments:
+if [ $# -gt 0 ]; then
+    for name in $@; do
+        echo "Looking for $name ..."
+        pid=$(ps -ef | grep $name | grep -v grep | awk '{print $2}')
+        echo "Found pid $pid"
+        if ! [ -z $pid ]; then
+            echo "Killing $name with pid $pid"
+            kill -9 $pid
+            while kill -0 $pid 2> /dev/null; do
+                sleep 1
+            done
+        fi
+    done
+else
+    # Otherwise, kill all in the list:
+    for bin in $binlist;
+    do
+        pid=$(ps -ef | grep $bin | grep -v grep | awk '{print $2}')
+        if ! [ -z $pid ]; then
+            echo "Killing $bin with pid $pid"
+            kill -9 $pid
+            while kill -0 $pid 2> /dev/null; do
+                sleep 1
+            done
+        fi
+    done
+fi
+
