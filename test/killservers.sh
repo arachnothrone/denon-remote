a06beb5593fd1e7e60d774f1a536486e9ab147c1
arachnothrone <arachnothrone@gmail.com>
2023-10-19 21:19:00 -0400

    Optimise kill script

--- test/killservers.sh
+++ test/killservers.sh
@@ -1,39 +1,28 @@
 #!/bin/sh
 
-# Kills all local running servers.
+# Kills all local running servers/clients. Takes process names from the 
+# predefined binlist if no arguments are given. 
+# Usage: killservers.sh [process_name1] [process_name2] ...
 
-binlist=("rpi_ag.out" "ir_agent.out")
-
-# Check names in cli arguments:
 if [ $# -gt 0 ]; then
-    echo "Process names to kill: $@"
-    for name in $@; do
-        echo "Looking for $name ..."
-        ps -ef | grep $name
-        echo "Debug: filtered output:"
-        ps -ef | grep $name | grep -v grep | grep -v killservers.sh
-        pid=$(ps -ef | grep $name | grep -v grep | grep -v killservers.sh | awk '{print $2}')
-        echo "Found pid $pid"
-        if ! [ -z $pid ]; then
-            echo "Killing $name with pid $pid"
-            kill -9 $pid
-            while kill -0 $pid 2> /dev/null; do
-                sleep 1
-            done
-        fi
-    done
+    binlist=$@
 else
-    # Otherwise, kill all in the list:
-    for bin in $binlist;
-    do
-        pid=$(ps -ef | grep $bin | grep -v grep | awk '{print $2}')
-        if ! [ -z $pid ]; then
-            echo "Killing $bin with pid $pid"
-            kill -9 $pid
-            while kill -0 $pid 2> /dev/null; do
-                sleep 1
-            done
-        fi
-    done
+    binlist=("rpi_ag.out" "ir_agent.out")
 fi
 
+echo "Process names to kill: $binlist"
+for name in $binlist; do
+    echo "Looking for $name ..."
+    ps -ef | grep $name
+    echo "Debug: filtered output:"
+    ps -ef | grep $name | grep -v grep | grep -v killservers.sh
+    pid=$(ps -ef | grep $name | grep -v grep | grep -v killservers.sh | awk '{print $2}')
+    echo "Found pid $pid"
+    if ! [ -z $pid ]; then
+        echo "Killing $name with pid $pid"
+        kill -9 $pid
+        while kill -0 $pid 2> /dev/null; do
+            sleep 1
+        done
+    fi
+done
