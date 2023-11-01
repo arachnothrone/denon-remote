50a1442cabb3ac9bb3086b35d8697d9baabf7be8
arachnothrone <arachnothrone@gmail.com>
2023-11-01 15:31:30 -0400

    Add power on command test with APO disabled

--- test/test_01.py
+++ test/test_01.py
@@ -44,7 +44,6 @@ def test_SERVER_START_STOP():
     assert "0,-42,0,2,0,0,1" in stdout2.decode()
 
 
-# @pytest.mark.skip(reason="Not implemented")
 def test_SERVER_START_AFTER_APO_TIME_WITH_APO_ENABLED():
     '''
     Start the server with APO time past current time,
@@ -112,10 +111,77 @@ def test_SERVER_START_AFTER_APO_TIME_WITH_APO_ENABLED():
         print(datetime.datetime.now(), "-" * 53)
 
 
-@pytest.mark.skip(reason="Not implemented")
+# @pytest.mark.skip(reason="Not implemented")
 def test_SERVER_START_BEFORE_APO_TIME_WITH_APO_DISABLED():
     '''
-    Start the server with APO time before current time,
-    send POWERON command and check if the power is ON
+    Start the server with APO past current time, send APO disable (CMD41AUTOPWROFF0)
+    then POWERON command and check if the power is still ON (server should ignore APO time)
     '''
-    pass
+
+    # Cleanup
+    out, err = tp.cleanup_environment(["rpi_ag.out", "client.py"])
+    tp.log_output(None, None, out, err)
+    time.sleep(2)
+
+    # Start 1 hour past APO time
+    server_command = ["../rpi_ag.out"]
+
+    client_command = ["python3", "../client.py"]
+    cliargs_a = ["CMD41AUTOPWROFF0", "127.0.0.1", "19001"]
+    cliargs_b = ["CMD04POWERON", "127.0.0.1", "19001"]
+    cliargs_c = ["CMD88SHUTDOWN", "127.0.0.1", "19001"]
+
+    for m_offset in [-1, 0, 1]:
+        hours, minutes, seconds = tp.get_local_time()
+
+        # Adjust to the next minute if too far into the current
+        if seconds > 25:
+            time.sleep(60 - seconds)
+            minutes += 1
+    
+        server_args = [f"-h {hours - 1}", f"-m {minutes + m_offset}", "-s 11", "-v -42"]
+
+        print(datetime.datetime.now(), f"Testing with minutes offset = {m_offset} (APO time: {hours - 1}:{minutes + m_offset}:11)")
+
+        # First thread for server, 2 - "CMD41AUTOPWROFF0" command, 3 - "CMD04POWERON", 4 - "CMD88SHUTDOWN"
+        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+            # Submit the task to run the process
+            future1 = executor.submit(tp.run_command, server_command, server_args)
+            future2 = executor.submit(tp.run_command, client_command, cliargs_a, delay=3)
+            future3 = executor.submit(tp.run_command, client_command, cliargs_b, delay=6)
+            future4 = executor.submit(tp.run_command, client_command, cliargs_c, delay=9)
+
+            # Wait for all tasks to complete
+            concurrent.futures.wait([future1, future2, future3, future4], timeout=60, return_when='ALL_COMPLETED')
+
+            # Get the result from the future
+            stdout1, stderr1 = future1.result()
+            stdout2, stderr2 = future2.result()
+            stdout3, stderr3 = future3.result()
+            stdout4, stderr4 = future4.result()
+
+        for stdout, stderr in [(stdout1, stderr1), (stdout2, stderr2), (stdout3, stderr3), (stdout4, stderr4)]:
+            tp.log_output(None, None, stdout, stderr)
+
+        # Server didn't try to turn off the power
+        assert f"Automatic Switch Off" not in stdout1.decode()
+
+        # Server received CMD41AUTOPWROFF0 request
+        assert "Response sent: 0,-42,0,2,0,0,0" in stdout1.decode()
+
+        # Server received CMD04POWERON requiest
+        assert "Response sent: 1,-42,0,2,0,0,0" in stdout1.decode()
+
+        # Server received CMD88SHUTDOWN request
+        assert "Response sent: 1,-42,0,2,0,0,0" in stdout1.decode()
+
+        # Check client thread 'CMD41AUTOPWROFF0' received response from server
+        assert "0,-42,0,2,0,0,0" in stdout2.decode()
+
+        # Check client thread 'CMD04POWERON' received response from server
+        assert "1,-42,0,2,0,0,0" in stdout3.decode()
+
+        # Check client 'CMD88SHUTDOWN' received response from server
+        assert "1,-42,0,2,0,0,0" in stdout4.decode()
+
+        print(datetime.datetime.now(), "-" * 53)
